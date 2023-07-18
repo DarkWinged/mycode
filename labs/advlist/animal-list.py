c006b12f8626b4bac12ce6915a3271f44553e275
DarkWinged <James.Levi.Rogers@gmail.com>
2023-07-18 20:56:53 +0000

    lab 17

---
+++ labs/advlist/animal-list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    animals = 'Fox Fly Ant Bee Cod Cat Dog Yak Cow Hen Koi Hog Jay Kit'.split()
+    for animal in animals:
+        print(animal, end=' ')
+    print()
+
