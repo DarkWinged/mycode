4060f0ccf50784fadc6e98d644a53ce628393b87
DarkWinged <James.Levi.Rogers@gmail.com>
2023-07-18 14:48:22 +0000

    completed bustedchallenge

---
+++ challanges/bustedchallenge.py
@@ -0,0 +1,9 @@
+#!/bin/env python3
+
+# Defined function
+def greet_user():
+    user_name = input("Please enter your name: ")
+    print(f"Hello, {user_name}!\nWelcome to Python class!")
+
+if __name__ == '__main__':
+    greet_user()
