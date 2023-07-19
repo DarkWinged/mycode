4802d0781614b8ed52e098fe6b145a8b3e720f3c
DarkWinged <James.Levi.Rogers@gmail.com>
2023-07-19 20:48:42 +0000

    added another question, reordered alignment tuple from (G,O) -> (O,G) to clarify question structure.

--- challanges/dnd_alignment_quiz.py
+++ challanges/dnd_alignment_quiz.py
@@ -3,20 +3,21 @@
 class Player:
     def __init__(self):
         name = ''
-        while name = '':
+        while name == '':
             name = input('Who are you?\n>')
         self.name = name
         self.alignment = (0, 0)
 
     def update_alignment(self, change: tuple[int, int]):
-        goodness_change, orderlyness_change = change
-        goodness, orderlyness = self.alignment
+        orderlyness_change, goodness_change = change
+        orderlyness, goodness = self.alignment
         goodness += goodness_change
         orderlyness += orderlyness_change
         self.alignment = (goodness, orderlyness)
 
     def print_alignment(self):
-        goodness, orderlyness = self.alignment
+        orderlyness, goodness = self.alignment
+
         if goodness <= -15:
             goodness = 'super evil'
         elif goodness <= -10:
@@ -71,32 +72,45 @@ def init():
     questions = []
     prompt = 'A young noble is beating their servant for failing some task. What do you do?'
     awnsers = {
-            'a': ('beat the noble and save the servant', (5,-5)),
-            'b': ('distact the noble alowing the servant to recover', (5,0)),
-            'c': ('offer to show the noble how to properly dicipline a servant', (0,5)),
-            'd': ('kill the servant to server as an example to the other servants', (-5,0))
+            'a': ('beat the noble and save the servant', (-5,5)),
+            'b': ('distact the noble alowing the servant to recover', (0,5)),
+            'c': ('offer to show the noble how to properly dicipline a servant', (5,0)),
+            'd': ('kill the servant to serve as an example to the other servants', (0,-5))
             } 
     questions.append(Question(prompt, awnsers))
     prompt = 'An orphanage is burning. What do you do?'
     awnsers = {
-            'a': ('rush in and save the orphans', (5,-5)),
+            'a': ('rush in and save the orphans', (-5,5)),
             'b': ('organize the townsfolk to putout the fire', (5,5)),
-            'c': ('revel in the destruction', (-5,0)),
+            'c': ('revel in the destruction', (0,-5)),
             'd': ('start more fires', (-5,-5))
             } 
     questions.append(Question(prompt, awnsers))
     prompt = 'A beggar comes up to you on the side of the road asking for alms. What do you do?'
     awnsers = {
             'a': ('give them a coin', (5,5)),
-            'b': ('share your rations', (5,0)),
+            'b': ('share your rations', (0,5)),
             'c': ('beat them up', (-5,-5)),
             'd': ('ignore them', (0,0))
+            } 
+    questions.append(Question(prompt, awnsers))
+    prompt = 'A wizard is asking for fresh bodies for study. What do you do?'
+    awnsers = {
+            'a': ('accuse him of being a necromancer and report him to the local lord', (5,5)),
+            'b': ('steal corpses form the cemetery', (-5,0)),
+            'c': ('negotiate a deal with the local lord to "dispose" of the bodies of executed criminals', (5, 0)),
+            'd': ('murder an innocent villager', (0,-5))
             } 
     questions.append(Question(prompt, awnsers))
     return questions
 
 if __name__ == '__main__':
     print('<----->D&D ALIGNMENT QUIZ<----->')
+    consent = ''
+    while consent != 'y':
+        consent = input('WARNING SOME QUESTIONS MY BE UNSUTABLE FOR SOME AUDIENCES\nprocede? (y/n): ').strip().lower()
+        if consent == 'n':
+            quit()
     questions = init()
     player = Player()
     for question in questions:
