468dbd9277df34ed13a66139aab452b52cb600c3
DarkWinged <James.Levi.Rogers@gmail.com>
2023-07-19 21:29:18 +0000

    added another question, fixed bug in alignment update function

--- challanges/dnd_alignment_quiz.py
+++ challanges/dnd_alignment_quiz.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+#Stores the player's name and their score.
+#Has methods to update and print a playe's score.
 class Player:
     def __init__(self):
         name = ''
@@ -13,37 +15,37 @@ class Player:
         orderlyness, goodness = self.alignment
         goodness += goodness_change
         orderlyness += orderlyness_change
-        self.alignment = (goodness, orderlyness)
+        self.alignment = (orderlyness, goodness)
 
     def print_alignment(self):
         orderlyness, goodness = self.alignment
 
-        if goodness <= -15:
+        if goodness <= -12:
             goodness = 'super evil'
-        elif goodness <= -10:
+        elif goodness <= -9:
             goodness = 'evil'
-        elif goodness <= -5:
+        elif goodness <= -6:
             goodness = 'a little evil'
-        elif goodness >= 15:
+        elif goodness >= 12:
             goodness = 'super good'
-        elif goodness >= 10:
+        elif goodness >= 9:
             goodness = 'good'
-        elif goodness >= 5:
+        elif goodness >= 6:
             goodness = 'a little good'
         else:
             goodness = 'neutal'
 
-        if orderlyness <= -15:
+        if orderlyness <= -12:
             orderlyness = 'super chaotic'
-        elif orderlyness <= -10:
+        elif orderlyness <= -9:
             orderlyness = 'chaotic'
-        elif orderlyness <= -5:
+        elif orderlyness <= -6:
             orderlyness = 'a little chaotic'
-        elif orderlyness >= 15:
+        elif orderlyness >= 12:
             orderlyness = 'super lawful'
-        elif orderlyness >= 10:
+        elif orderlyness >= 9:
             orderlyness = 'lawful'
-        elif orderlyness >= 5:
+        elif orderlyness >= 6:
             orderlyness = 'a little lawful'
         else:
             orderlyness = 'neutal'
@@ -51,6 +53,9 @@ class Player:
         print(f'{self.name} is {orderlyness} {goodness}')
 
 
+#Stores a prompt and the valid awnsers to the prompt.
+#Prompt awnsers also contain score values.
+#Has methods that allow a player to awnser the prompt and recieve a score.
 class Question:
     def __init__(self, prompt: str, awnsers:dict[str: tuple[str, tuple[int, int]]]):
         self.prompt = prompt
@@ -68,49 +73,76 @@ class Question:
                 player.update_alignment(self.awnsers[player_awnser][1])
                 invalid = False
 
+
+#initalizes and returns a question list.
+"""
+new questions can be added by following this template: 
+    prompt = 'Blah bla, bla bla... Blah. What do you do?'
+    awnsers = {
+            'a': ('blah', (orderly: int, goodly: int)),
+            'b': ('blah', (orderly: int, goodly: int)),
+            'c': ('blah', (orderly: int, goodly: int)),
+            'd': ('blah', (orderly: int, goodly: int))
+            } 
+    questions.append(Question(prompt, awnsers))
+"""
 def init():
     questions = []
     prompt = 'A young noble is beating their servant for failing some task. What do you do?'
     awnsers = {
-            'a': ('beat the noble and save the servant', (-5,5)),
-            'b': ('distact the noble alowing the servant to recover', (0,5)),
-            'c': ('offer to show the noble how to properly dicipline a servant', (5,0)),
-            'd': ('kill the servant to serve as an example to the other servants', (0,-5))
+            'a': ('beat the noble and save the servant', (-3,3)),
+            'b': ('distact the noble alowing the servant to recover', (0,3)),
+            'c': ('offer to show the noble how to properly dicipline a servant', (3,0)),
+            'd': ('kill the servant to serve as an example to the other servants', (0,-3))
             } 
     questions.append(Question(prompt, awnsers))
     prompt = 'An orphanage is burning. What do you do?'
     awnsers = {
-            'a': ('rush in and save the orphans', (-5,5)),
-            'b': ('organize the townsfolk to putout the fire', (5,5)),
-            'c': ('revel in the destruction', (0,-5)),
-            'd': ('start more fires', (-5,-5))
+            'a': ('rush in and save the orphans', (-3,3)),
+            'b': ('organize the townsfolk to putout the fire', (3,3)),
+            'c': ('revel in the destruction', (0,-3)),
+            'd': ('start more fires', (-3,-3))
             } 
     questions.append(Question(prompt, awnsers))
     prompt = 'A beggar comes up to you on the side of the road asking for alms. What do you do?'
     awnsers = {
-            'a': ('give them a coin', (5,5)),
-            'b': ('share your rations', (0,5)),
-            'c': ('beat them up', (-5,-5)),
+            'a': ('give them a coin', (3,3)),
+            'b': ('share your rations', (0,3)),
+            'c': ('beat them up', (-3,-3)),
             'd': ('ignore them', (0,0))
             } 
     questions.append(Question(prompt, awnsers))
     prompt = 'A wizard is asking for fresh bodies for study. What do you do?'
     awnsers = {
-            'a': ('accuse him of being a necromancer and report him to the local lord', (5,5)),
-            'b': ('steal corpses form the cemetery', (-5,0)),
-            'c': ('negotiate a deal with the local lord to "dispose" of the bodies of executed criminals', (5, 0)),
-            'd': ('murder an innocent villager', (0,-5))
+            'a': ('accuse him of being a necromancer and report him to the local lord', (3,3)),
+            'b': ('steal corpses form the cemetery', (-3,0)),
+            'c': ('negotiate a deal with the local lord to "dispose" of the bodies of executed criminals', (3, 0)),
+            'd': ('murder an innocent villager', (0,-3))
+            } 
+    questions.append(Question(prompt, awnsers))
+    prompt = 'A dragon is terrorizing a nearby village. What do you do?'
+    awnsers = {
+            'a': ('gather you allies and slay the dargon', (3,3)),
+            'b': ('offer a treasure as tribute to dragon in exchange for the village\s safety', (0,3)),
+            'c': ('sacrafice a young prince to the dragon in exchange for power', (0, -3)),
+            'd': ('use your bardic charm to seduce the dragon <3', (-3,0))
             } 
     questions.append(Question(prompt, awnsers))
     return questions
 
-if __name__ == '__main__':
-    print('<----->D&D ALIGNMENT QUIZ<----->')
+
+#Consent form must be seigned in the affermative inorder to take the test
+def consent_form():
     consent = ''
     while consent != 'y':
         consent = input('WARNING SOME QUESTIONS MY BE UNSUTABLE FOR SOME AUDIENCES\nprocede? (y/n): ').strip().lower()
         if consent == 'n':
             quit()
+
+
+if __name__ == '__main__':
+    print('<----->D&D ALIGNMENT QUIZ<----->')
+    consent_form()
     questions = init()
     player = Player()
     for question in questions:
