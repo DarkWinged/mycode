8a8dd87a1325fd480c192cb00ef268a200574a8e
DarkWinged <James.Levi.Rogers@gmail.com>
2023-07-19 15:22:31 +0000

    finished dict_challange

--- challanges/dict_challange.py
+++ challanges/dict_challange.py
@@ -1,9 +1,12 @@
-#!/usr/bin/evn python3
+#!/usr/bin/env python3
 
-import pprint
+from pprint import pprint
 
-hugh_jackman ={}
-hugh_jackman['Citizenship'] = ['Australia', 'United Kingdom']
-hugh_jackman['Born'] = {'name': 'Hugh Michael Jackman', 'DoB': '12 October 1968', 'age': 'age 54', 'location': 'Sydney, New South Wales, Australia'}
-hugh_jackman['Occupations_info'] = {'Occupation': ['Actor', 'Singer'], 'Years active': '1994–present'}
+if __name__ == '__main__':
+    hugh_jackman ={}
+    hugh_jackman['Citizenship'] = ['Australia', 'United Kingdom']
+    hugh_jackman['Born'] = {'name': 'Hugh Michael Jackman', 'DoB': '12 October 1968', 'age': 'age 54', 'location': 'Sydney, New South Wales, Australia'}
+    hugh_jackman['Occupations_info'] = {'Occupation': ['Actor', 'Singer'], 'Years active': '1994–present', 'Awards':'https://en.wikipedia.org/wiki/List_of_awards_and_nominations_received_by_Hugh_Jackman'}
+    
+    pprint(hugh_jackman)
 
