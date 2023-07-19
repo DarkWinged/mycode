343145eb19e30930a9b5c6d35d83abed45209512
DarkWinged <James.Levi.Rogers@gmail.com>
2023-07-19 14:49:59 +0000

    7-19-2023 morning challanges

---
+++ challanges/dayly_challange_01.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import random
+from time import ctime
+
+def init():
+    wordbank= ["indentation", "spaces"]
+    tlgstudents= ['Alex', 'Benji', 'Cayla', 'Demetra', 'Derek', 'Deshawn', 'James', 'Maria', 'Marylyn', 'Nor', 'Sal', 'Sammy']
+    wordbank.append(4)
+    return wordbank, tlgstudents
+
+def get_input():
+    num = input('Which studen are we talking about?\n>')
+    num_strs = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    is_int = False
+
+    for num_str in num_strs:
+        if num_str in num:
+            num = int(num)
+            is_int = True
+            break
+    
+    return num, is_int
+
+def find_student(students: list[str], id_val: any, id_type: bool) -> str:
+    if id_type:
+        return students[id_val]
+    if id_val in students:
+        return students[students.index(id_val)]
+    return id_val
+
+def print_student(student: str, wordbank: list[any]):
+    print(f'{student} always uses {wordbank[2]} {wordbank[1]} to indent.')
+
+def rand_student(students: list[str]) -> str:
+    random.seed(ctime())
+    num = int((random.random()*100) % len(students))
+    return students[num]
+
+if __name__ == '__main__':
+    wordbank, tlgstudents = init()
+    num, is_int = get_input()
+    student = find_student(tlgstudents, num, is_int)
+    print_student(student, wordbank)
+    student = rand_student(tlgstudents)
+    print_student(student, wordbank)
+
---
+++ challanges/dict_challange.py
@@ -0,0 +1,9 @@
+#!/usr/bin/evn python3
+
+import pprint
+
+hugh_jackman ={}
+hugh_jackman['Citizenship'] = ['Australia', 'United Kingdom']
+hugh_jackman['Born'] = {'name': 'Hugh Michael Jackman', 'DoB': '12 October 1968', 'age': 'age 54', 'location': 'Sydney, New South Wales, Australia'}
+hugh_jackman['Occupations_info'] = {'Occupation': ['Actor', 'Singer'], 'Years active': '1994–present'}
+
