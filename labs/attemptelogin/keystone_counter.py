27a469d7a5bf9ef51d8401493bb41496fafc1cad
DarkWinged <James.Levi.Rogers@gmail.com>
2023-07-21 15:38:38 +0000

    lab_35

---
+++ labs/attemptelogin/keystone_counter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+def load_data() -> list[str]:
+    with open('keystone.common.wsgi') as file:
+        return file.readlines()
+def process_data(data: list[str]) -> tuple[int,list[str],int]:
+    fail_ip = []
+    sucess = 0
+    
+    for entry in data:
+        if 'failed' in entry:
+            fail_ip.append(entry.split()[-1])
+        elif 'post' in entry:
+            sucess += 1
+
+    return fail_ip, sucess
+
+if __name__ == "__main__":
+    ips, sucesses = process_data(load_data())
+    print(f'posts: {sucesses}')
+    print(f'authentication failures: {len(ips)}')
+    for ip in ips:
+        print(f' {ip}')
+
