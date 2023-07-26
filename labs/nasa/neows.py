33acdd4b10a6ea4b03c86a148c3bcb4501dde5cf
DarkWinged <James.Levi.Rogers@gmail.com>
2023-07-26 16:32:14 +0000

    updated neows to find the most threating neo

--- labs/nasa/neows.py
+++ labs/nasa/neows.py
@@ -3,10 +3,40 @@
 
 #!/usr/bin/python3
 import requests
+import math
 from pprint import pprint
 
 ## Define NEOW URL
 NEOURL = "https://api.nasa.gov/neo/rest/v1/feed?"
+
+
+def estimated_sphere_volume(min_diameter, max_diameter):
+    # Calculate the average diameter
+    avg_diameter = (min_diameter + max_diameter) / 2.0
+
+    # Calculate the radius
+    radius = avg_diameter / 2.0
+
+    # Calculate the volume of the sphere
+    volume = (4/3) * math.pi * (radius ** 3)
+
+    return volume
+
+
+def calculate_rank(volume, speed_kph, miss_distance_k):
+    # Define weights for each criterion
+    volume_weight = 0.4
+    speed_weight = 0.3
+    distance_weight = 0.3
+
+    # Calculate the rank score
+    rank_score = (
+        volume_weight * volume +
+        speed_weight * (1 / speed_kph) +
+        distance_weight * (1 / miss_distance_k)
+    )
+
+    return rank_score
 
 # this function grabs our credentials
 # it is easily recycled from our previous script
@@ -41,19 +71,30 @@ def main():
         for neo in range(len(neos[date])):
             if neos[date][neo]['is_potentially_hazardous_asteroid']:
                 hazard = neos[date][neo]
+                diameter = hazard['estimated_diameter']['meters']
+                volume = estimated_sphere_volume(diameter['estimated_diameter_min'], diameter['estimated_diameter_max'])
                 hazards.append({'name':hazard['name'],
                     'date':date,
-                    'estimated_diameter_in_meters':hazard['estimated_diameter']['meters'],
-                    'close_approach_data':hazard['close_approach_data']
+                    'estimated_diameter_in_meters':diameter,
+                    'estimated_volume': volume,
+                    'close_approach_data':hazard['close_approach_data'][0],
+                    'threat': calculate_rank(volume,
+                        float(hazard['close_approach_data'][0]['relative_velocity']['kilometers_per_hour']),
+                        float(hazard['close_approach_data'][0]['miss_distance']['kilometers']))
                     })
+               
     
-    for hazard in hazards:
+    sorted_hazards = sorted(hazards, key=lambda x: x['threat'], reverse=True)
+    '''for hazard in sorted_hazards:
         print('{')
         pprint(hazard['name'])
         pprint(hazard['date'])
+        pprint(hazard['threat'])
         pprint(hazard['estimated_diameter_in_meters'])
+        pprint(hazard['estimated_volume'])
         pprint(hazard['close_approach_data'])
-        print('}')
+        print('}')'''
+    pprint(sorted_hazards[0])
 
     ## display NASAs NEOW data
     #pprint(neodata['near_earth_objects']['2019-11-18'][0].keys())
