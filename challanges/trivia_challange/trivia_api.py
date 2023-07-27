76491f537b199730a12099e34d27a488baa87ee0
DarkWinged <James.Levi.Rogers@gmail.com>
2023-07-27 15:33:01 +0000

    trivia challange removed debug code

--- challanges/trivia_challange/trivia_api.py
+++ challanges/trivia_challange/trivia_api.py
@@ -54,7 +54,6 @@ def main():
     # data will be a python dictionary rendered from your API link's JSON!
     print(url)
     data = requests.get(url).json()['results']
-    pprint(data)    
     questions = [Question(quest) for quest in data]
     for question in questions:
         question.ask()
