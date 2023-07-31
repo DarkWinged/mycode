e267b991d6eae38ecfc09ce6affc59992cf781ae
DarkWinged <James.Levi.Rogers@gmail.com>
2023-07-31 13:56:35 +0000

    updated formatting

--- challanges/flaskapi/flaskgame.py
+++ challanges/flaskapi/flaskgame.py
@@ -6,23 +6,6 @@ import os
 import json
 from flask import Flask, render_template, request, redirect, url_for, session
 
-app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Change this to a secure secret key
-
-data_dir = os.path.expanduser('~/server_data')
-data_file_path = os.path.join(data_dir, 'logins.json')
-
-# Create data directory and logins.json file with default admin login if they don't exist
-if not os.path.exists(data_dir):
-    os.makedirs(data_dir)
-
-if not os.path.exists(data_file_path):
-    default_admin_login = {
-        "admin": "password"
-    }
-    with open(data_file_path, 'w') as f:
-        json.dump(default_admin_login, f)
-
 class DataController:
     def __init__(self, file_path):
         self._file_path = file_path
@@ -53,76 +36,90 @@ class DataController:
 
         return False
 
-data_controller = DataController(data_file_path)
+if __name__ == '__main__':
 
-def is_user_logged_in():
-    return 'username' in session
+    app = Flask(__name__)
+    app.secret_key = 'your_secret_key'  # Change this to a secure secret key
 
-@app.route('/', methods=['GET'])
-def index():
-    if is_user_logged_in():
-        username = session['username']
-        return render_template('index.html', username=username)
-    else:
+    data_base_path = os.path.expanduser('~/server_data')
+    # Create data directory and logins.json file with default admin login if they don't exist
+    if not os.path.exists(data_base_path):
+        os.makedirs(f'{data_base_path}/logins.json')
+
+    if not os.path.exists(data_base_path):
+        default_admin_login = {
+            "admin": "password"
+        }
+        with open(data_file_path, 'w') as f:
+            json.dump(default_admin_login, f)
+
+    data_controller = DataController(f'{data_base_path}/logins.json')
+
+
+    @app.route('/', methods=['GET'])
+    def index():
+        if 'username' in session.keys():
+            username = session['username']
+            return render_template('index.html', title_text = 'Flask Game!', username=username)
+        else:
+            return redirect(url_for('login'))
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        form = {
+            'title': 'Flask Game!',
+            'fields': [
+                {'label': 'Username:', 'name': 'username', 'type': 'text'},
+                {'label': 'Password:', 'name': 'password', 'type': 'password'}
+            ],
+            'switch_text': 'Need an account? Signup here!',
+            'switch_route': 'signup',
+            'button_text': 'Login'
+        }
+
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+
+            if data_controller.validate_user_credentials(username, password):
+                session['username'] = username  # Store username in session
+                return redirect(url_for('index'))
+            else:
+                return render_template('login_signup.html', form=form, message="Invalid credentials. Please try again.")
+
+        return render_template('login_signup.html', form=form)
+
+    @app.route('/signup', methods=['GET', 'POST'])
+    def signup():
+        form = {
+            'title': 'Flask Game!',
+            'fields': [
+                {'label': 'New Username:', 'name': 'new_username', 'type': 'text'},
+                {'label': 'New Password:', 'name': 'new_password', 'type': 'password'},
+                {'label': 'Confirm Password:', 'name': 'confirm_password', 'type': 'password'}
+            ],
+            'button_text': 'Sign Up',
+            'switch_text': 'Already have an account?',
+            'switch_route': 'login'
+        }
+
+        if request.method == 'POST':
+            new_username = request.form['new_username']
+            new_password = request.form['new_password']
+            confirm_password = request.form['confirm_password']
+
+            if data_controller.add_new_account(new_username, new_password, confirm_password):
+                return redirect(url_for('login'))
+            else:
+                return render_template('login_signup.html', form=form, message="Failed to create an account. Please try again.")
+
+        return render_template('login_signup.html', form=form)
+
+    @app.route('/logout', methods=['POST'])
+    def logout():
+        session.clear()
         return redirect(url_for('login'))
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = {
-        'title': 'Login',
-        'fields': [
-            {'label': 'Username:', 'name': 'username', 'type': 'text', 'required': True},
-            {'label': 'Password:', 'name': 'password', 'type': 'password', 'required': True}
-        ],
-        'button_text': 'Login',
-        'switch_text': 'Sign Up',
-        'switch_route': 'signup'
-    }
 
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        if data_controller.validate_user_credentials(username, password):
-            session['username'] = username  # Store username in session
-            return redirect(url_for('index'))
-        else:
-            return render_template('login_signup.html', form=form, message="Invalid credentials. Please try again.")
-
-    return render_template('login_signup.html', form=form)
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = {
-        'title': 'Sign Up',
-        'fields': [
-            {'label': 'New Username:', 'name': 'new_username', 'type': 'text', 'required': True},
-            {'label': 'New Password:', 'name': 'new_password', 'type': 'password', 'required': True},
-            {'label': 'Confirm Password:', 'name': 'confirm_password', 'type': 'password', 'required': True}
-        ],
-        'button_text': 'Sign Up',
-        'switch_text': 'Login',
-        'switch_route': 'login'
-    }
-
-    if request.method == 'POST':
-        new_username = request.form['new_username']
-        new_password = request.form['new_password']
-        confirm_password = request.form['confirm_password']
-
-        if data_controller.add_new_account(new_username, new_password, confirm_password):
-            return redirect(url_for('login'))
-        else:
-            return render_template('login_signup.html', form=form, message="Failed to create an account. Please try again.")
-
-    return render_template('login_signup.html', form=form)
-
-@app.route('/logout', methods=['POST'])
-def logout():
-    session.clear()
-    return redirect(url_for('login'))
-
-if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2224, debug=True)
-
 
