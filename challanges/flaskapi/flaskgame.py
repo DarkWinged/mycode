983ac88b7f6f69ba3ac2242acb287deaf0a03ea2
DarkWinged <James.Levi.Rogers@gmail.com>
2023-08-01 01:35:35 +0000

    added admin dashboard links to the user managment page

--- challanges/flaskapi/flaskgame.py
+++ challanges/flaskapi/flaskgame.py
@@ -58,9 +58,58 @@ class LoginDataController:
         else:
             return False
 
-
     def get_usernames(self):
         return list(self._data.keys())
+
+    def reset_user_password(self, admin_username: str, admin_password: str, new_password: str, confirm_password: str, username: str) -> bool:
+        # Check if the admin password is valid
+        if not self.validate_user_credentials(admin_username, admin_password):
+            return False
+
+        # Check if new password and confirm password are the same
+        if new_password != confirm_password:
+            return False
+
+        # Here, you can perform any additional checks or validations as needed before changing the password
+
+        # Update the user's password to the new one
+        if self._data.get(username):
+            self._data[username]['password'] = new_password
+            self._save_data()
+            return True
+
+        return False
+    
+    def _add_permission(self, username: str, permission: str) -> bool:
+        if permission in self._data[username]['permissions']:
+            return True
+        else:
+            self._data[username]['permissions'].append(permission)
+            self._save_data()
+            return True
+        return False
+   
+    def _remove_permission(self, username: str, permission: str) -> bool:
+        if permission not in self._data[username]['permissions']:
+            return True
+        else:
+            self._data[username]['permissions'].pop(self._data[username]['permissions'].index(permission))
+            self._save_data()
+            return True
+        return False
+
+    def set_user_permissions(self, admin_username: str, admin_password: str, action: str, permission: str, username: str) -> bool:
+        # Check if the admin password is valid
+        if not self.validate_user_credentials(admin_username, admin_password):
+            return False
+        
+        if action == 'add':
+            return self._add_permission(username, permission)
+        
+        if action == 'remove':
+            return self._remove_permission(username, permission)
+
+
 
 if __name__ == '__main__':
 
@@ -94,7 +143,7 @@ if __name__ == '__main__':
         if 'username' in session:
             if request.endpoint in login_pages:
                 return redirect('/')
-            elif request.endpoint == 'admin':
+            elif 'admin' in request.endpoint:
                 if not login_data_controller.user_has_permission(session['username'], 'admin'):
                     return redirect(url_for('index'))
         else:
@@ -286,6 +335,42 @@ if __name__ == '__main__':
             # Render the template and set cookies using the helper function
             return render_admin_users_template(filtered_usernames, total_filtered_users, current_search, limit, current_offset, max_offset)
 
+
+    @app.route('/admin/users/<username>', methods=['GET', 'POST'])
+    def admin_user_options(username):
+        if request.method == 'POST':
+            data = request.get_json()
+            if data and 'admin_password' in data:
+                admin_password = data['admin_password']
+
+                # Handle reset password functionality
+                if 'reset_password' in data:
+                    passwords = data['reset_password']
+                    if login_data_controller.reset_user_password(session['username'], admin_password, passwords['new_password'], passwords['confirm_password'], username):
+                        return render_template('admin_user_options.html', username=username, message="Password reset successful")
+                    else:
+                        return render_template('admin_user_options.html', username=username, error="Password reset failed")
+
+                # Handle set permissions functionality
+                if 'set_permissions' in data:
+                    # Check if selected_permission is a valid permission value
+                    action = list(data['set_permissions'].keys())[0]
+                    permission = data['set_permissions'][action]
+                    allowed_permissions = ['user', 'moderator', 'admin']
+                    if login_data_controller.set_user_permissions(session['username'], admin_password, action, permission, username):
+                        return render_template('admin_user_options.html', username=username, message=f"{permission.capitalize()} permissions set")
+                    else:
+                        return render_template('admin_user_options.html', username=username, error="Invalid permission selected")
+
+                # Handle other possible actions based on the form data
+
+            else:
+                # Handle the case when no admin password was provided in the form submission
+                return render_template('admin_user_options.html', username=username, error="Please enter admin password")
+
+        else:
+            # Render the admin_users_username.html template with the current user's username
+            return render_template('admin_user_options.html', username=username)
 
     app.run(host='0.0.0.0', port=2224, debug=True)
 
