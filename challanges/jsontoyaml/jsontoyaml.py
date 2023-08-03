39bb8cd68e2ba7e96365b1ca309b47769c114fe8
DarkWinged <James.Levi.Rogers@gmail.com>
2023-08-03 14:55:03 +0000

    jsontoyaml challange added new entries argument

--- challanges/jsontoyaml/jsontoyaml.py
+++ challanges/jsontoyaml/jsontoyaml.py
@@ -11,10 +11,10 @@ URL = 'https://raw.githubusercontent.com/csfeeser/Python/master/data%20sets/favs
 
 def add_new_entry() -> dict[str, str]:
     new_entry = {
-        'name': input('name:'), 
-        'movie': input('movie:'), 
-        'color': input('color:'), 
-        'ice cream': input('ice cream:')}    
+        'name': input('\tname:'), 
+        'movie': input('\tmovie:'), 
+        'color': input('\tcolor:'), 
+        'ice cream': input('\tice cream:')}    
     return new_entry
 
 def write_yaml(data: list[dict[str, str]]):
@@ -26,13 +26,16 @@ def write_yaml(data: list[dict[str, str]]):
     
 def main():
     data = requests.get(URL).json()
-    data.append(add_new_entry())
+    for entry in range(args.new_entries):
+        print(f'- {entry}:')
+        data.append(add_new_entry())
     write_yaml(data)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Gets a json from a url then converts it to a yaml file")
     parser.add_argument("path", nargs='?', type=str, help="Path to where the yaml file will be saved.")
+    parser.add_argument('-n', '--new_entries', type=int, default=1, help='Number of new entries to add to the data')
     args = parser.parse_args()
-    if args.path:
+    if args.path and args.new_entries > 0:
         main()
