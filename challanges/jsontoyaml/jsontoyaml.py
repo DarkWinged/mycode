15c86400d13585b536c09698e9fb8b9e25b53156
DarkWinged <James.Levi.Rogers@gmail.com>
2023-08-03 14:39:29 +0000

    jsontoyaml challange updated

--- challanges/jsontoyaml/jsontoyaml.py
+++ challanges/jsontoyaml/jsontoyaml.py
@@ -9,21 +9,26 @@ from os import path
 URL = 'https://raw.githubusercontent.com/csfeeser/Python/master/data%20sets/favs.json'
 
 
-def main():
-    data = requests.get(URL).json()
-    pprint(data)
+def add_new_entry() -> dict[str, str]:
     new_entry = {}
     new_entry['name'] = input('name:') 
     new_entry['movie'] = input('movie:') 
     new_entry['color'] = input('color:') 
     new_entry['ice cream'] = input('ice cream:')
-    data.append(new_entry)
+    return new_entry
+
+def write_yaml(data: list[dict[str, str]]):
     file_path = args.path
     if not file_path.endswith('.yaml'):
         file_path = file_path + '.yaml'
     with open(path.expanduser(path.normpath(file_path)), "w") as yaml_file:
         yaml.dump(data, yaml_file)
     
+def main():
+    data = requests.get(URL).json()
+    data.append(add_new_entry())
+    write_yaml(data)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Gets a json from a url then converts it to a yaml file")
